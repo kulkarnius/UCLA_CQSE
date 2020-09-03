eec6eb640efb2c0e329e4362a814e2f81e50923a
kulkarnius <64239278+kulkarnius@users.noreply.github.com>
2020-09-02 19:04:58 -0600

    Updates

--- xml_class.py
+++ xml_class.py
@@ -8,13 +8,29 @@ class xml_parser:
         self.tree = ET.parse(fileName)
         self.root = self.tree.getroot()
 
-    def getParameterList(self,parameterListName):
+    def getTagList(self,parameterListName):
         parameterList = self.tree.find(parameterListName)
+        if parameterList  == None:
+            raise Exception("Not a valid parameterListName", parameterListName)
+        parameters = [child.tag for child in parameterList]
+        return parameterList.tag, parameters
 
-        parameters = []
+    def getAttribsList(self,parameterListName):
+        parameterList = self.tree.find(parameterListName)
+        if parameterList  == None:
+            raise Exception("Not a valid parameterListName", parameterListName)
+        parameters = [child.attrib for child in parameterList]
+        return parameterList.tag, parameters
 
-        for child in parameterList:
-            parameters.append(child)
-
-        return parameterList, parameters
+    def getParameterValues(self, parameterName):
+        children = [(child.tag, child.attrib) for child in self.root.iter(parameterName)]
+        if self.root.iter(parameterName) == None:
+            raise Exception("Not a valid parameterName", parameterName)
+        return children
+    
+    def getChildValues(self, parameterList):
+        childrenTags = [[(child.tag, child.attrib) for child in elem] for elem in self.root.iter(parameterList)]
+        if self.root.iter(parameterList) == None:
+            raise Exception("Not a valid parameterName", parameterList)
+        return childrenTags
     
