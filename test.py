c8ec6846e1a039036fb039b53ad4dc7227283c47
kulkarnius <64239278+kulkarnius@users.noreply.github.com>
2020-09-07 14:18:45 -0700

    Sweeping Changes

--- test.py
+++ test.py
@@ -1,11 +1,29 @@
 import xml.etree.ElementTree as ET
+import os
 
 
 tree = ET.parse('PsiOpDev.xml')
 root = tree.getroot()
 
 
-def getType():
+def biasSweep():
+    mins = [float(elem.get('value')) for elem in root.findall(".//min")]
+    maxs = [float(elem.get('value')) for elem in root.findall(".//max")]
+    incre = [float(elem.get('value')) for elem in root.findall(".//increment")]
+    
+    currentPath = os.getcwd()
+    os.mkdir(currentPath + '/sweepFiles')
 
+    for n in range(1 + int((maxs[0]-mins[0])/incre[0])):
+        topBias = mins[0] + n*incre[0]
+        root.find('.//GateBias/topBias').set('value', str(topBias))
+        for i in range(1 + int((maxs[1]-mins[1])/incre[1])):
+            botBias = mins[1] + i*incre[1]
+            root.find('.//GateBias/botBias').set('value', str(botBias))
+            tree.write('sweepFiles/output{}{}.xml'.format(str(n),str(i)))
+    
+    return 
 
-print(getLayer("Layer5"))
+    
+
+print(biasSweep())
--- xml_class.py
+++ xml_class.py
@@ -1,6 +1,7 @@
 
 
 import xml.etree.ElementTree as ET 
+import os
 
 
 class xml_parser:
@@ -46,4 +47,22 @@ class xml_parser:
                 childrenTags = [(child.tag, child.attrib) for child in self.root[3][i+1].iter()]
                 return childrenTags
 
-    
+    def biasSweep(self):
+        mins = [float(elem.get('value')) for elem in self.root.findall(".//min")]
+        maxs = [float(elem.get('value')) for elem in self.root.findall(".//max")]
+        incre = [float(elem.get('value')) for elem in self.root.findall(".//increment")]
+        
+        currentPath = os.getcwd()
+        os.mkdir(currentPath + '/sweepFiles')
+
+        for n in range(1 + int((maxs[0]-mins[0])/incre[0])):
+            topBias = mins[0] + n*incre[0]
+            self.root.find('.//GateBias/topBias').set('value', str(topBias))
+            for i in range(1 + int((maxs[1]-mins[1])/incre[1])):
+                botBias = mins[1] + i*incre[1]
+                self.root.find('.//GateBias/botBias').set('value', str(botBias))
+                self.tree.write('sweepFiles/output{}{}.xml'.format(str(n),str(i)))
+        
+        return 
+
+        
