645f5552ef1abcba8b09191a6bc32a419f2ebfd1
kulkarnius <64239278+kulkarnius@users.noreply.github.com>
2020-09-18 22:23:51 -0700

    Single Sweep Added in

--- test.py
+++ test.py
@@ -3,12 +3,33 @@ import xml_class as xm
 templateFile = xm.xml_parser("test.xml")
 sweepFile = xm.xml_parser("testSweep.xml")
 
-def generateSweepLists():
+def generateSweeps(subDirectoryName):
     mins = sweepFile.getValues("min")
     maxs = sweepFile.getValues("max")
     incres = sweepFile.getValues("increment")
+    
 
-    if len(mins) == 1:
+    if xm.subDirectoryExistence(subDirectoryName) != True: #Checks to see if Directory exists
+        xm.createSubDirectory("/{}".format(subDirectoryName))
+
+    # Eventually want to build-in functionality that lets us directily overwrite the entire directory
+
+    """
+    Hierachy of Checks:
+        Check for Links
+        Check for Multiple Nested Loops
+    """
+
+    if len(mins) == 1: #The case for single sweeps.
         for n in range(1 + int((maxs[0]-mins[0])/incres[0])):
-            topBias = mins[0] + n*incres[0]
-            templateFile.setValue()
+            value = xm.truncate(mins[0] + n*incres[0])
+            templateFile.setValue("paramA",value)
+            templateFile.writeToFile('{}/output{}.xml'.format(subDirectoryName, str(n)))
+    elif len(mins) > 1:
+        
+ 
+
+parents = sweepFile.getParent("min")
+print(parents)
+
+
--- xml_class.py
+++ xml_class.py
@@ -2,12 +2,21 @@
 
 import xml.etree.ElementTree as ET 
 import os
-import itertool as it
+import itertools as it
+import math
 
 
 def createSubDirectory(subDirectoryName):
     currentPath = os.getcwd()
     os.mkdir(currentPath + '/{}'.format(subDirectoryName))
+
+def subDirectoryExistence(subDirectoryName):
+    currentPath = os.getcwd()
+    return os.path.isdir(currentPath + "/{}".format(subDirectoryName))
+
+def truncate(number):
+    factor = 10.0 ** 2
+    return math.trunc(number * factor) / factor
 
 class xml_parser:
     def __init__(self, fileName):
@@ -15,13 +24,21 @@ class xml_parser:
         self.root = self.tree.getroot()
 
     def getValues(self,valueName):
-        return [float(elem.get("value")) for elem in self.root.findall(".//{})".format(valueName))]
+        return [float(elem.get("value")) for elem in self.root.findall(".//{}".format(valueName))]
     
     def setValue(self, targetValuePath, value):
         self.root.find('.//{}'.format(targetValuePath)).set('value', str(value))
 
     def getParent(self, valueName):
-        return self.root.find(".//{}..)".format(valueName))
+        return [elem.tag for elem in self.root.findall(".//{}/..".format(valueName))]
+    
+
+    def writeToFile(self, outputFilePath):
+        self.tree.write(outputFilePath)
+    
+    def getRunParameters(self):
+        return [elem.tag for elem in self.root.findall('.//runParameters/*')]
+
 
     
 
