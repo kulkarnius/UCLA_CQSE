1198fad1d14db555dfdd14155280b3f1f8d1a2b0
kulkarnius <64239278+kulkarnius@users.noreply.github.com>
2020-09-19 22:10:52 -0700

    Updates

--- test.py
+++ test.py
@@ -9,12 +9,14 @@ def generateSweeps(subDirectoryName):
     incres = sweepFile.getValues("increment")
     parents = sweepFile.getParent("min")
 
-    vals = [mins, maxs, incres]
+    sweeps = [[xm.truncate(mins[i] + n*incres[i]) for n in range(1 + int((maxs[i]-mins[i])/incres[i]))] for i in range(len(mins))]
     
+    combinations = xm.nestedLoops(sweeps)
+
 
     if xm.subDirectoryExistence(subDirectoryName) != True: #Checks to see if Directory exists
         xm.createSubDirectory("/{}".format(subDirectoryName))
-
+    
     # Eventually want to build-in functionality that lets us directily overwrite the entire directory
 
     """
@@ -30,8 +32,11 @@ def generateSweeps(subDirectoryName):
             templateFile.setValue("paramA",value)
             templateFile.writeToFile('{}/output{}.xml'.format(subDirectoryName, str(n)))
     elif len(mins) > 1:
-        for i in range(len(mins)):
-            values = [xm.truncate(mins[0] + n*incres[0])]
+        for i in range(len(combinations)):
+            for j in range(len(parents)):
+                templateFile.setValue(parents[j],combinations[i][j])  #Need to add valid path checking to here at some point
+                templateFile.writeToFile('{}/output{}{}.xml'.format(subDirectoryName, str(i),str(j)))
 
 
-print(xm.nestedLoops()
+generateSweeps("cheese")
+
